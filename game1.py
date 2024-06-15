f32dce32dd2cf300b80dd7af013f02a162a038bc
Abhijeet <130231006+abhijeetsara@users.noreply.github.com>
2024-06-15 21:13:03 +0530

    Create game1.py
    it is a game we are adding to pakages  random 2 - time and using sleep() functionn time.sleep(3) its cahching hour real time

---
+++ game1.py
@@ -0,0 +1,11 @@
+#its a game make for fun
+
+import random
+import time
+
+print("\nWelcome to Hangman game by Data Flaire\n")
+name = input("Enter your name 0")
+print("hello"+name+"Best of luck ")
+time.sleep(2)
+print("The game is about to start ! \n let's play Hnagama ")
+time.sleep(10)
